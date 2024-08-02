dd6afed1cb87f2e2f600ece9f4a95f217207dff0
Otavio <correaotavio0409@gmail.com>
2024-08-02 12:39:53 -0300

    Ponteiros e array

---
+++ Relembrando Ponteiros/PtrArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int main()
+{
+	setlocale(LC_ALL, "PORTUGUESE");
+
+	int arr[5]{ 1, 2, 3, 4, 5 };
+
+	int* ptr = arr;
+
+	std::cout << "Posição na memória de arr[5] " << &arr << '\n';
+	std::cout << "Oque esta dentro de arr[5] " << arr << '\n';
+
+
+	std::cout << "Posição na RAM de ptr " << &ptr[5] << sizeof(arr) << '\n';
+	std::cout << "\n Tamanho da varivel arr[5] " << sizeof(arr) << '\n';
+
+	for (int i = 0; i < 5; i++) {
+		std::cout << "\t\tTamanho de cada posição do array " << sizeof(ptr[5]) << " Bytes" << '\n';
+	}
+
+	std::cout << "Onde *ptr está localizado na memória? " << &ptr << '\n';
+	std::cout << "Verificando oque imprime dento de ptr: " << ptr << '\n';
+}
